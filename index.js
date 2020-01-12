3dc626ab3e8e03fec47e8863a9ac099cc8f53780
Sadat Toktosunova <saadatt@gmail.com>
2020-01-11 19:39:37 -0600

    Course Project on Unit Tests_most_recently_updated

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-//function that sorts out a given array in ascending order
+//1) function that sorts out a given array in ascending order
 function sortArrayAscend(arr) {
   return arr.sort((a, b) => a - b);
 }
 
-//function that checks if a word is a palindrome
+//2) function that checks if a word is a palindrome
 function isPalindrome(str) {
   return str.toLowerCase().split('').reverse('').join('') === str.toLowerCase();
 }
 
-//Function that checks if an integer is odd 
+//3) Function that checks if an integer is odd
 function isOdd(num) {
   return  num % 2 !== 0;
 }
 
-//function that checks if an integer is even
+//4) function that checks if an integer is even
 function isEven(num) {
   if (num % 2 === 0) return true;
 }
 
-//function that checks if a person is an adult
+//5) function that checks if a person is an adult
 function isAdult(num) {
   if (num < 0) {
     return 'not born yet';
@@ -27,7 +27,7 @@ function isAdult(num) {
   } return 'false';
 }
 
-//CREDIT CARD MASK: Imagine that you are making a purchase online and entering your credit card number into the
+//6) CREDIT CARD MASK: Imagine that you are making a purchase online and entering your credit card number into the
 // required empty field. While you are doing so, you notice that the all of the digits
 // of your credit card number except the last four, appear masked with '#'. Your task now is to write a function
 // to maskify credit card numbers of other people by changing all but the last four characters into '#'.
@@ -40,18 +40,18 @@ function protecting(ccnum) {
   return c.repeat(ccnum.length - 4) + a;
 }
 
-// function that checks if the string ends with the given ending
+//7) function that checks if the string ends with the given ending
 function isEnding(str, ending){
   return str.endsWith(ending);
 }
 
-//function that returns the square root of an integer
+//8) function that returns the square root of an integer
 function squareRoot(n) {
   if (n < 0) {return 'NaN';}
   return Math.sqrt(n);
 }
 
-//Given a string, you have to return a string in which each character (case-sensitive) is repeated once
+//9) Given a string, you have to return a string in which each character (case-sensitive) is repeated once
 function letterRepeat(str) {
   let b =[], a;
   a = str.split('');
